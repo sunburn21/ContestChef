00b0b200eb8a65cad4b82118c05030ae100c2dc5
sunburn21 <sunnythesingh21@gmail.com>
2020-03-31 01:52:42 +0530

    sdf

--- src/App.js
+++ src/App.js
@@ -42,8 +42,8 @@ class App extends Component{
       <Router>
       <div className="App">
         <div style={{position:"fixed",left:'0',top:'0',fontSize:'30px',display:'flex',width:'150px',justifyContent:'space-around'}}> 
-        <Link style={{color:'#333333'}} to='/home'> <IoIosHome/></Link>
-        <Link style={{color:'#333333'}} to='/login'><FaSignInAlt/><span style={{fontSize:'18px'}}>Login</span> </Link>
+        <Link style={{color:'#333333'}} to='/contestchef/home'> <IoIosHome/></Link>
+        <Link style={{color:'#333333'}} to='/contestchef/login'><FaSignInAlt/><span style={{fontSize:'18px'}}>Login</span> </Link>
         </div>
         <Switch>
           <Route path='/contestchef/home' exact  render={(props)=>isLoggedIn?<Home {...props} user = {this.state.user} />:<LoginPage {...props} />}/>;

d1ceeee253b1863928a6faac8487ac6e99558864
sunburn21 <sunnythesingh21@gmail.com>
2020-03-31 00:50:55 +0530

    Updates

---
+++ precache-manifest.512e842e0c63aecece9e883ad144010f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d91a7093f9fdf7d049e1147effc9aa19",
+    "url": "/contestchef/index.html"
+  },
+  {
+    "revision": "8d29791faea28920d4c7",
+    "url": "/contestchef/static/css/2.36b4f1ac.chunk.css"
+  },
+  {
+    "revision": "6c582e54f528c63e2bb2",
+    "url": "/contestchef/static/css/main.3079944f.chunk.css"
+  },
+  {
+    "revision": "8d29791faea28920d4c7",
+    "url": "/contestchef/static/js/2.5e38d60e.chunk.js"
+  },
+  {
+    "revision": "919ae2601bae895ca25f85d93270e93c",
+    "url": "/contestchef/static/js/2.5e38d60e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c582e54f528c63e2bb2",
+    "url": "/contestchef/static/js/main.5a4aa6f0.chunk.js"
+  },
+  {
+    "revision": "99924e7e8f7b6687f835",
+    "url": "/contestchef/static/js/runtime-main.4512d3a5.js"
+  }
+]);
